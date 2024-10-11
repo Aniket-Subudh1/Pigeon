900efa683b7c404985e47041443aac111e325f6e
Durga1234-cloud <puhandurgasankarp@gmail.com>
2024-10-11 18:08:54 +0530

    add log in page

---
+++ app/src/main/java/com/example/piegeon/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.piegeon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Additional login logic can be added here (e.g., handling user inputs, authenticating)
+    }
+}
--- app/src/main/java/com/example/piegeon/SplashActivity.kt
+++ app/src/main/java/com/example/piegeon/SplashActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -14,7 +13,6 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
 
         // Apply window insets for edge-to-edge experience
@@ -24,10 +22,9 @@ class SplashActivity : AppCompatActivity() {
             insets
         }
 
-        // Use a Handler to delay the transition to MainActivity for 2 seconds
+        // Use a Handler to delay the transition to LoginActivity for 4 seconds
         Handler(Looper.getMainLooper()).postDelayed({
-            // Start MainActivity after the splash screen duration
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(intent)
             finish() // Close the SplashActivity so it doesn't remain in the back stack
         }, splashScreenDuration)
