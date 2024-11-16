15cc58ddd7eeea0a4ff9525f1f975051f570907f
BIBH123 <bibhuprasadpradhan82@gmail.com>
2024-11-16 19:46:16 +0530

    add chart list repo

--- app/src/main/java/com/example/pigeon/mvvm/ChatListRepo.kt
+++ app/src/main/java/com/example/pigeon/mvvm/ChatListRepo.kt
@@ -1,4 +1,78 @@
-package com.example.pigeon.mvvm
+package com.example.chatmessenger.mvvm
 
-class ChatListRepo {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.pigeon.Utils
+import com.example.pigeon.modal.RecentChats
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+
+
+class ChatListRepo() {
+
+
+    val firestore = FirebaseFirestore.getInstance()
+
+
+    fun getAllChatList(): LiveData<List<RecentChats>> {
+
+        val mainChatList = MutableLiveData<List<RecentChats>>()
+
+
+        // SHOWING THE RECENT MESSAGED PERSON ON TOP
+        firestore.collection("Conversation${Utils.getUidLoggedIn()}").orderBy("time", Query.Direction.DESCENDING)
+            .addSnapshotListener { snapshot, exception ->
+
+
+                if (exception != null) {
+
+                    return@addSnapshotListener
+                }
+
+
+                val chatlist = mutableListOf<RecentChats>()
+
+                snapshot?.forEach { document ->
+
+                    val chatlistmodel = document.toObject(RecentChats::class.java)
+
+
+                    if (chatlistmodel.sender.equals(Utils.getUidLoggedIn())) {
+
+
+                        chatlistmodel.let {
+
+
+                            chatlist.add(it)
+
+
+
+
+
+
+
+
+                        }
+
+
+                    }
+
+
+
+
+
+
+
+                }
+
+
+                mainChatList.value = chatlist
+
+
+            }
+
+        return mainChatList
+
+
+        }
 }
