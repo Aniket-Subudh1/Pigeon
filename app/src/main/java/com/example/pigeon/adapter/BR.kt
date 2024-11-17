4030876f58850e4349ce0bd8e2a3aea3f08a6924
Swagat-D <swagatdash164@gmail.com>
2024-11-18 00:20:09 +0530

    minir changes

---
+++ app/src/main/java/com/example/pigeon/adapter/BR.kt
@@ -0,0 +1,22 @@
+package com.example.pigeon.adapter
+
+.class public Lcom/example/chatmessenger/BR;
+.super Ljava/lang/Object;
+.source "BR.java"
+
+
+# static fields
+.field public static final _all:I = 0x0
+
+.field public static final viewModel:I = 0x1
+
+
+# direct methods
+.method public constructor <init>()V
+.registers 1
+
+.line 3
+invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+
+return-void
+.end method
